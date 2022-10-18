cdc9c5f874d6c04434705841e20f18d2114a68ab
Jan Wagner <waja@cyconet.org>
2022-10-18 15:33:36 +0200

    trying to fix this again

--- local.php
+++ local.php
@@ -1,5 +1,4 @@
 <?php
 
-@define('CONST_DATABASE_DSN', 'pgsql:dbname=nominatim;host=postgres;port=5432;user=postgres');
-
-@define('CONST_DATABASE_DSN', '/data/flatnode.file');
+define('CONST_DATABASE_DSN', 'pgsql:dbname=nominatim;host=postgres;port=5432;user=postgres');
+define('CONST_OSM2PGSQL_FLATNODE_FILE', '/data/flatnode.file');
